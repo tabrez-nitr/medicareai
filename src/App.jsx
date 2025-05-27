13fea5ffd7e6a97e5cb87da4cacc175ef5d1b7bb
tabrez <04tabrez@gmail.com>
2025-05-27 14:14:18 +0530

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { ClassNames } from '@emotion/react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+         <h2>Medi Field making easy for user</h2>
+         <h2>Storing medical Records</h2>
+         <h2>AI powered percription and report analysis</h2>
+         <h2>Notification for medicine dosage</h2>
+         <h2>Record your parents mediacl situation with every day health report so you can for your loved ones</h2>
+         <h2>Reminder for checks ups and tests</h2>
+    </div>
+  )
+}
+
+export default App
