d99cb51216549065c4a738344b12d69fe5f4b4d1
tabrez <04tabrez@gmail.com>
2026-01-04 18:48:29 +0530

    feat : project setup

---
+++ frontend/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>MediCareAi</h1>
+      <p>Welcome to MediCareAi.</p>
+    </div>
+  );
+}
